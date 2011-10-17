426d96238db70668243f508659542f3c6b44d6db
SHIN Suzuki <shinout310+git@gmail.com>
2011-10-17 21:10:13 +0900

    fixed a bug: port number cannot be set

--- u2r.js
+++ u2r.js
@@ -2,6 +2,7 @@ module.exports = function(url, ret) {
   if (typeof url != "string") {
     throw new Error("type of url must be string");
   }
+  if (url.indexOf("http") == -1) url = "http://" + url;
   var op = require('url').parse(url);
   if (!op.hostname) {
     if (op.pathname.charAt(0) != '/') {
@@ -12,7 +13,7 @@ module.exports = function(url, ret) {
   }
   ret = ret || {};
   ret.host = op.hostname;
-  ret.port = op.port || (op.protocol == 'https:') ? 443 : 80;
+  ret.port = op.port || ((op.protocol == 'https:') ? 443 : 80);
   ret.path = ( op.pathname 
               ?  ((op.pathname.slice(0,1) == "/") ? "" : "/") + op.pathname + (op.search || "") 
               : "/");
